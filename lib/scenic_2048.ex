3b3f7eef0c7d5fd2346b5ed9b9cca3945dfbb8bc
Thomas Coopman <thomas.coopman@gmail.com>
2018-09-08 12:50:00 +0200

    initial commit

---
+++ lib/scenic_2048.ex
@@ -0,0 +1,20 @@
+defmodule Scenic2048 do
+  @moduledoc """
+  Starter application using the Scenic framework.
+  """
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # load the viewport configuration from config
+    main_viewport_config = Application.get_env(:scenic_2048, :viewport)
+
+    # start the application with the viewport
+    children = [
+      supervisor(Scenic2048.Sensor.Supervisor, []),
+      supervisor(Scenic, [viewports: [main_viewport_config]]),
+    ]
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
+end
